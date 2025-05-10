7fd449d42212d2cb2af1d787b8c1282c01c88a0e
Jivan052 <jamadarjivan01@gmail.com>
2025-05-10 23:18:48 +0530

    _

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { collection, query, orderBy, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
 import { FaChevronDown, FaChevronUp, FaPuzzlePiece, FaHistory, FaExternalLinkSquareAlt } from 'react-icons/fa';
-import QuizCard from '../reusables/Quizcard';
+import QuizCard from '../reusables/QuizCard';
 
 const QuizPage = () => {
   const [activeQuizzes, setActiveQuizzes] = useState([]);
